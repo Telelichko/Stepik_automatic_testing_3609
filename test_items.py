36926148e718ad46a8545c79cc48791c7636733f
Telelichko <teleleichko@yandex.ru>
2019-07-27 02:42:21 +0700

    Commiting date: 27.07.2019. Changed files conftest.py and test_items.py

--- test_items.py
+++ test_items.py
@@ -1,4 +1,5 @@
 ﻿from selenium import webdriver
+import time
 
 def get_link():    # Метод, передающий ссылку на сайт
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -8,6 +9,7 @@ def get_link():    # Метод, передающий ссылку на сайт
 def test_should_be_button_basket(browser):
     link = get_link()    # Получение ссылки 
     browser.get(link)    # Передача ссылки в метод get
+    time.sleep(30)    # Ожидание - для проверки текста в кнопке
     assert browser.find_element_by_css_selector("button.btn-add-to-basket"), "There isn't button basket on the site"    # Проверка
 
 
