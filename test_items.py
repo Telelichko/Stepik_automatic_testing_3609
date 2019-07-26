a174c25b844818cbcbc1d845071882e3fa1589da
Telelichko <teleleichko@yandex.ru>
2019-07-27 02:29:43 +0700

    Commiting date: 27.07.2019. Added files conftest.py and test_items.py

---
+++ test_items.py
@@ -0,0 +1,14 @@
+﻿from selenium import webdriver
+
+def get_link():    # Метод, передающий ссылку на сайт
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    return link
+
+
+def test_should_be_button_basket(browser):
+    link = get_link()    # Получение ссылки 
+    browser.get(link)    # Передача ссылки в метод get
+    assert browser.find_element_by_css_selector("button.btn-add-to-basket"), "There isn't button basket on the site"    # Проверка
+
+
+ 
